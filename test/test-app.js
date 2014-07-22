8a70014ac64ed38f1f7ac3eee28b2187359271b0
colster <colyork@gmail.com>
2014-07-22 19:36:15 +0800

    Basic testing framework added

---
+++ test/test-app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by colinyork on 21/07/2014.
+ */
+angular.module('app', ['ngResource']);
+
+var toastr = {};
+
---
+++ test/tests/account/muUserSpec.js
@@ -0,0 +1,23 @@
+/**
+ * Created by colinyork on 22/07/2014.
+ */
+
+describe('mvUser', function() {
+
+    beforeEach(module('app'));
+
+    describe('isAdmin' , function() {
+
+        it('should return false if the roles array does not have an admin entry', inject(function(mvUser) {
+            var user = new mvUser();
+            user.roles = ['not admin'];
+            expect(user.isAdmin()).to.be.false;
+        }));
+
+        it('should return true if the roles array has an admin entry', inject(function(mvUser) {
+            var user = new mvUser();
+            user.roles = ['admin'];
+            expect(user.isAdmin()).to.be.true;
+        }))
+   })
+});
